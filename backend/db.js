409c771290173258c5b2bf218523ab096e603de3
sangram chopdar <sangramdadasochopdar@gmail.com>
2023-11-25 00:30:05 +0530

    mongoDb Added

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb://127.0.0.1:27017/InoteBook";
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to MongoDB database");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+module.exports = connectToMongo;
