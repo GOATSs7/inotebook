1a43f6dd9f80027b32e5274ca0e46e4c8d9597e3
sangram chopdar <sangramdadasochopdar@gmail.com>
2023-11-26 18:11:09 +0530

    user creat added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-
+const { body, validationResult } = require("express-validator");
 //creat user using post
 
-router.post("/", (req, res) => {
-  console.log(req.body);
+router.post(
+  "/",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "password must be 6 Characters ").isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
 
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
-});
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then((user) => res.json(user))
+      .catch(
+        (err) => console.log(err),
+        res.json({ error: "Please enter a unique value for email" })
+      );
+  }
+);
 
 module.exports = router;
